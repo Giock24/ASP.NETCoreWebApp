ddc3c582ad2b7f986a021783ac43f2aee39d4868
Gianluca Consoli <gianluca.consoli@newteam.it>
2025-04-09 11:47:29 +0200

    Added css to .html file

--- Test5/Test5/obj/Debug/net8.0/Test5.AssemblyInfo.cs
+++ Test5/Test5/obj/Debug/net8.0/Test5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2cdb7ee2cead156ced5474234755d5c7b6885d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3cef7d5501d6f231d9abcdbd10df7830c7b4023")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
